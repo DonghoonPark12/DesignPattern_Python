5132c0f286fc203923c933df41a58c2b4150924e
Park Donghun <donghunp1212@gmail.com>
2023-01-08 15:48:51 +0900

    add SingletonPattern.py

---
+++ SingletonPattern.py
@@ -0,0 +1,61 @@
+from threading import Lock, Thread
+
+class SingletonMeta(type):
+    """
+    The Singleton class can be implemented in different ways in Python. Some
+    possible methods include: base class, decorator, metaclass. We will use the
+    metaclass because it is best suited for this purpose.
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class Singleton(metaclass=SingletonMeta):
+    def some_business_logic(self):
+        """
+        Finally, any singleton should define some business logic, which can be
+        executed on its instance.
+        """
+
+        # ...
+
+if __name__ == "__main__":
+    # The client code.
+
+    s1 = Singleton()
+    s2 = Singleton()
+
+    if id(s1) == id(s2):
+        print("Singleton works, both variables contain the same instance.")
+    else:
+        print("Singleton failed, variables contain different instances.")
+
+    '''
+    Singleton works, both variables contain the same instance.
+    '''
+
+    # The client code.
+    print("If you see the same value, then singleton was reused (yay!)\n"
+          "If you see different values, "
+          "then 2 singletons were created (booo!!)\n\n"
+          "RESULT:\n")
+
+    process1 = Thread(target=test_singleton, args=("FOO",))
+    process2 = Thread(target=test_singleton, args=("BAR",))
+    process1.start()
+    process2.start()
+    '''
+    If you see the same value, then singleton was reused (yay!)
+    If you see different values, then 2 singletons were created (booo!!)
+    FOO
+    FOO
+    '''
